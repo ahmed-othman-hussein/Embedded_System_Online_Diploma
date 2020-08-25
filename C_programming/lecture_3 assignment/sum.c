a15721e719831d1587e98cd96610705ebb91610e
ahmed othman <ahmedothman@gmail.com>
2020-08-25 23:57:28 +0200

    sum

---
+++ C_programming/lecture_3 assignment/sum.c
@@ -0,0 +1,18 @@
+/*
+ * sum.c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+
+void main(){
+	int x,y;
+	printf("Enter two integers ");
+	fflush(stdout);
+	scanf("%d\n%d",&x,&y);
+	printf("sum: %d ",x+y);
+
+
+}
