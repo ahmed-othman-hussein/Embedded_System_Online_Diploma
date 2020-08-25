bee9f83962f331d2a40f69332da1acd27630bda9
ahmed othman <ahmedothman@gmail.com>
2020-08-26 00:13:50 +0200

    ASCII value

---
+++ C_programming/lecture_3 assignment/ASCII_value.c
@@ -0,0 +1,18 @@
+/*
+ * ASCII_Value.c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+
+void main(){
+	char x;
+	printf("Enter a character: ");
+	fflush(stdout);
+	scanf("%c",&x);
+	printf("ASCII value of %c = %d ",x,x);
+
+
+}
