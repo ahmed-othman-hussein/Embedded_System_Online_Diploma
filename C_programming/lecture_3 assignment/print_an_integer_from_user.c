d93463806e438c5ae28c8f3846c24c0bf4b02547
ahmed othman <ahmedothman@gmail.com>
2020-08-25 23:21:07 +0200

    printing integer

---
+++ C_programming/lecture_3 assignment/print_an_integer_from_user.c
@@ -0,0 +1,18 @@
+/*
+ * print_an_integer_from_user.c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+
+void main(){
+	int x;
+	printf("Enter an integer ");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("you Entered: %d ",x);
+
+
+}
