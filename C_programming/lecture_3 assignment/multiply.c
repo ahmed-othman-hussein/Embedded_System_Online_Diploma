9ae15269d79dca3a91222d8211a2fc7d3a9d1b11
ahmed othman <ahmedothman@gmail.com>
2020-08-26 00:03:17 +0200

    multiply

---
+++ C_programming/lecture_3 assignment/multiply.c
@@ -0,0 +1,18 @@
+/*
+ * multiplying.c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+
+void main(){
+	float x,y;
+	printf("Enter two numbers ");
+	fflush(stdout);
+	scanf("%f\n%f",&x,&y);
+	printf("Product: %f ",x*y);
+
+
+}
