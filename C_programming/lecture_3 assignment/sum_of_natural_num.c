32797f127b0942d5d75eee7050ce1f36f4c0e663
ahmed othman <ahmedothman@gmail.com>
2020-08-26 06:25:54 +0200

    sum of natural numbers

---
+++ C_programming/lecture_3 assignment/sum_of_natural_num.c
@@ -0,0 +1,26 @@
+/*
+ * sum of natural numbers.c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+
+void main(){
+	int  x,i,sum=0;
+	printf("Enter an integer: ");
+	fflush(stdout);
+	scanf("%d",&x);
+
+	for (i=1;i<=100;i++){
+		sum+=i;
+	}
+	printf("sum = %d ",sum);
+
+
+}
+
+
+
+
