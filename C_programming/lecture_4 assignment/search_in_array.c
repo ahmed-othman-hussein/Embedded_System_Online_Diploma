d7210be937a961ea5c148b934b42a06fe2143d0c
ahmed othman <ahmedothman@gmail.com>
2020-09-07 08:05:29 +0200

    searching array

---
+++ C_programming/lecture_4 assignment/search_in_array.c
@@ -0,0 +1,39 @@
+/*
+ * search in the array .c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+
+int main(){
+	int i,x,e;
+	int a[20] ;
+
+	printf("Enter the numbers of elements:");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&x);
+
+	for(i=0;i<x;i++){
+
+		fflush(stdout);fflush(stdin);
+		scanf("%d",&a[i]);
+
+
+	}
+	printf("Enter the element to be searched:");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&e);
+
+	for(i=0;i<x;i++){
+		if (a[i]==e){
+			printf("number found at  location = %d",i+1);
+
+		}
+
+	}
+
+
+	return 0;
+}
