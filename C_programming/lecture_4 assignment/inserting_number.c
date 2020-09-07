f39314177c89fd1042843ecadbf83a44b9dd296a
ahmed othman <ahmedothman@gmail.com>
2020-09-07 07:38:15 +0200

    inserting number to array

---
+++ C_programming/lecture_4 assignment/inserting_number.c
@@ -0,0 +1,40 @@
+/*
+ * insert number to array .c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+
+int main(){
+	int i,x,e,l;
+	int a[20] ;
+
+	printf("Enter the numbers of elements:");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&x);
+
+	for(i=0;i<x;i++){
+
+		fflush(stdout);fflush(stdin);
+		scanf("%d",&a[i]);
+
+
+	}
+	printf("Enter the element to be inserted:");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&e);
+	printf("Enter the location:");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&l);
+	for(i=x;i>=l;i--){
+		a[i]=a[i-1];
+	}
+	a[l-1]=e;
+	for(i=0;i<x+1;i++){
+
+		printf("%d",a[i]);
+	}
+	return 0;
+}
