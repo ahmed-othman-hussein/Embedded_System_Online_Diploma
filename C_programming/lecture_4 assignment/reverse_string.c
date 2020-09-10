4e3f7fd39f84f91703678b8b22d4f34343db2cc9
ahmed othman <ahmedothman@gmail.com>
2020-09-10 03:08:47 +0200

    reverse_string

---
+++ C_programming/lecture_4 assignment/reverse_string.c
@@ -0,0 +1,29 @@
+/*
+ * reverse string .c
+ *
+ *  Created on: Aug 25, 2020
+ *      Author: ahmed
+ */
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int i,x,j=0;
+	char t[50],c[50] ;
+
+	printf("Enter a string:");
+	fflush(stdout);fflush(stdin);
+	gets(t);
+	x=strlen(t);
+	j=x-1;
+
+	for(i=0;i<x;i++){
+		c[j]=t[i];
+		j--;
+	}
+
+	c[j]='\0';
+	printf("reverse string is:%s",c);
+
+	return 0;
+}
